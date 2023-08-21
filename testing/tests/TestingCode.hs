b833aff21249fed0a36966ae93861492f32a440f
Nkalla <elieraoulnet@gmail.com>
2023-08-21 17:30:21 +0100

    configuration des tests dans un pgojet cabal, tout marche bien, je me suis inspiré de Week02.cabal, et aussi de la documentation officielle de cabal

--- testing/tests/TestingCode.hs
+++ testing/tests/TestingCode.hs
@@ -2,24 +2,31 @@
 -- le module de testing doit exporter tests
 -- le nom de la fonction de testing doit etre tests 
 
-module TestingCode  (tests ) where 
+-- module TestingCode  (tests ) where 
+
+module Main  where 
 
 import Test.Hspec 
-import Distribution.TestSuite.QuickCheck 
+--import Distribution.TestSuite.QuickCheck 
 import Test.QuickCheck 
 import Control.Exception
 
 import TestFonctions 
 
--- tests :: IO ()
--- tests = hspec $ do 
---     describe "test de kìla fonction addition " $ do 
---         it "(addition a b) >= a" $ do 
---             (addition 2 5) >= 2 `shouldBe` True  
 
 
-tests :: IO [Test ]
-tests = return [testProperty "addition est la fonction dont je veux tester" someProperty ]
+main :: IO ()
+main = hspec $ do 
+    describe "test de kìla fonction addition " $ do 
+        it "(addition a b) >= a" $ do 
+            (addition 2 5) >= 2 `shouldBe` True  
 
-someProperty :: Integer -> Integer -> Bool 
-someProperty a b = addition a b == a + b
+-- main :: Int -> Int -> Bool 
+-- main x y = (addition x y) == x + y 
+
+
+-- tests :: IO [Test ]
+-- tests = return [testProperty "addition est la fonction dont je veux tester" someProperty ]
+
+-- someProperty :: Integer -> Integer -> Bool 
+-- someProperty a b = addition a b == a + b
